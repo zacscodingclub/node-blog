f7c4addf6755ecbc7695b0010c7692c59e8a6aca
zacscodingclub <zbaston@gmail.com>
2016-09-15 16:12:50 -0400

    small updates fixing syntax.  remove unneeded users.js file generated via express

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var mongo = require('mongodb');
 var db = require('monk')('localhost/node-blog');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -69,7 +69,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
