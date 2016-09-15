f1447b388ea3704b0428b40a527e482a5a7be10f
zacscodingclub <zbaston@gmail.com>
2016-09-15 10:20:22 -0400

    include needed dependencies and setup middleware in app.js

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var flash = require('connect-flash');
+var moment = require('moment');
+
+var mongo = require('mongodb');
+var db = require('monk')('localhost/node-blog');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +30,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
